697a84a989c7bba65cde0c841903680f99e99bea
Lakshmipriyaloki <Lakshmipriyaloki@gmail.com>
2024-12-03 18:51:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+ 
+def sneha(request):
+    return HttpResponse('sneha Is A Innocent girl who is very kind hearted')
